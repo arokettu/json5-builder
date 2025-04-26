c1aefa4b28f38b9b50c1641cf301ecc345ade0da
Anton Smirnov <sandfox+git@sandfox.me>
2025-04-26 04:16:23 +0300

    Mark as internal

--- src/Values/RawJson5Serializable.php
+++ src/Values/RawJson5Serializable.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Arokettu\Json5\Values;
 
+/**
+ * @internal Use at your own risk
+ */
 interface RawJson5Serializable
 {
     public function getRawJson5(): string;
