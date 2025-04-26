13dee59f3840ec050b558a54e3198c4cf7ffe03b
Anton Smirnov <sandfox+git@sandfox.me>
2025-04-26 08:02:12 +0300

    Test hex edge cases

--- tests/Scalars/IntegerTest.php
+++ tests/Scalars/IntegerTest.php
@@ -18,10 +18,20 @@ class IntegerTest extends TestCase
 
     public function testHexInt(): void
     {
-        self::assertEquals("0x123\n", Json5Encoder::encode(new HexInteger(0x123)));
-        self::assertEquals("-0x123\n", Json5Encoder::encode(new HexInteger(-0x123)));
+        self::assertEquals("0x123ACD\n", Json5Encoder::encode(new HexInteger(0x123acd)));
+        self::assertEquals("-0x123ACD\n", Json5Encoder::encode(new HexInteger(-0x123acd)));
 
         // JSON is supported too
-        self::assertEquals("291", json_encode(new HexInteger(0x123)));
+        self::assertEquals("1194701", json_encode(new HexInteger(0x123acd)));
+
+        // 0
+        self::assertEquals("0x0\n", Json5Encoder::encode(new HexInteger(0)));
+
+        // PHP_INT_MAX
+        $expect = '7F' . str_repeat('FF', PHP_INT_SIZE - 1);
+        self::assertEquals("0x{$expect}\n", Json5Encoder::encode(new HexInteger(PHP_INT_MAX)));
+        self::assertEquals("-0x{$expect}\n", Json5Encoder::encode(new HexInteger(-PHP_INT_MAX)));
+
+        // todo: handle PHP_INT_MIN
     }
 }
