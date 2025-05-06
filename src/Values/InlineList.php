217dda1d29cc71540b93acdfbac66413f6091f18
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-06 10:51:35 +0300

    Inline structures prototype

---
+++ src/Values/InlineList.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Json5\Values;
+
+final class InlineList
+{
+    use Internal\IterableValueTrait;
+    use Internal\IterableValueListTrait;
+}
