c7d90e3f0c3312168c45698bc773df44aac0036d
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-06 11:09:15 +0300

    Use set hook to validate indent

--- src/Options.php
+++ src/Options.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace Arokettu\Json5;
 
-use Error;
 use ValueError;
 
 /**
@@ -17,8 +16,6 @@ use ValueError;
  */
 final class Options
 {
-    private string $indent;
-
     public function __construct(
         // strings
         public Options\Quotes $keyQuotes = Options\Quotes::Double,
@@ -29,37 +26,16 @@ final class Options
         // floats
         public bool $preserveZeroFraction = false,
         // formatting
-        string $indent = '    ', // todo: property hook in PHP 8.4+
+        public string $indent = '    ' {
+            set {
+                if (!preg_match('/^[\x20\x09\x0a\x0d]*$/', $value)) {
+                    throw new ValueError('Indent must contain only whitespace characters');
+                }
+                $this->indent = $value;
+            }
+        },
         public bool $inlineArrayPadding = false,
         public bool $inlineObjectPadding = true,
     ) {
-        $this->setIndent($indent);
-    }
-
-    private function setIndent(string $indent): void
-    {
-        if (!preg_match('/^[\x20\x09\x0a\x0d]*$/', $indent)) {
-            throw new ValueError('Indent must contain only whitespace characters');
-        }
-        $this->indent = $indent;
-    }
-
-    public function __set(string $name, mixed $value): void
-    {
-        if ($name === 'indent') {
-            $this->setIndent($value);
-            return;
-        }
-
-        throw new Error('No such property: ' . $name);
-    }
-
-    public function __get(string $name): mixed
-    {
-        if ($name === 'indent') {
-            return $this->indent;
-        }
-
-        throw new Error('No such property: ' . $name);
     }
 }
