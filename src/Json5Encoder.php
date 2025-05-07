a802ca0c427a10c1888f7451abd14bb2e2b17df6
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-07 19:08:34 +0300

    Syntax update

--- src/Json5Encoder.php
+++ src/Json5Encoder.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Arokettu\Json5;
 
-final class Json5Encoder
+final readonly class Json5Encoder
 {
     public static function encode(mixed $value, Options $options = new Options()): string
     {
         $stream = fopen('php://temp', 'r+');
-        (new Engine\Encoder($value, $options, $stream))->encode();
+        new Engine\Encoder($value, $options, $stream)->encode();
         rewind($stream);
         $json5 = stream_get_contents($stream);
         fclose($stream);
