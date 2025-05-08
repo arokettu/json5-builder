0930e42a5847e0f2d9c62ab5cc2738886159c9c7
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-08 21:57:33 +0300

    All empty should be collapsed

--- tests/Formatting/EmptyTest.php
+++ tests/Formatting/EmptyTest.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace Arokettu\Json5\Tests\Formatting;
 
 use Arokettu\Json5\Json5Encoder;
-use ArrayObject;
+use Arokettu\Json5\Values\CompactList;
+use Arokettu\Json5\Values\CompactObject;
+use Arokettu\Json5\Values\InlineList;
+use Arokettu\Json5\Values\InlineObject;
+use Arokettu\Json5\Values\ObjectValue;
 use PHPUnit\Framework\TestCase;
 
 class EmptyTest extends TestCase
@@ -13,10 +17,14 @@ class EmptyTest extends TestCase
     public function testEmptyList(): void
     {
         self::assertEquals("[]\n", Json5Encoder::encode([]));
+        self::assertEquals("[]\n", Json5Encoder::encode(new InlineList([])));
+        self::assertEquals("[]\n", Json5Encoder::encode(new CompactList([])));
     }
 
     public function testEmptyObject(): void
     {
-        self::assertEquals("{}\n", Json5Encoder::encode(new ArrayObject()));
+        self::assertEquals("{}\n", Json5Encoder::encode(new ObjectValue([])));
+        self::assertEquals("{}\n", Json5Encoder::encode(new InlineObject([])));
+        self::assertEquals("{}\n", Json5Encoder::encode(new CompactObject([])));
     }
 }
