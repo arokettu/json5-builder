396527c18d39950111ded7bc1d8b59a354b6555c
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-13 22:51:39 +0300

    Write directly to a stream

---
+++ tests/EncodingFormatsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Json5\Tests;
+
+use Arokettu\Json5\Json5Encoder;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use TypeError;
+
+class EncodingFormatsTest extends TestCase
+{
+    public function testStream(): void
+    {
+        $data = ['a' => 'b'];
+
+        $resource = tmpfile();
+        Json5Encoder::encodeToStream($resource, $data);
+        rewind($resource);
+        self::assertEquals("{\n    a: \"b\",\n}\n", stream_get_contents($resource));
+        fclose($resource);
+    }
+
+    public function testNotResource(): void
+    {
+        self::expectException(TypeError::class);
+        self::expectExceptionMessage('$stream must be a writable stream');
+        Json5Encoder::encodeToStream(new stdClass(), null);
+    }
+}
