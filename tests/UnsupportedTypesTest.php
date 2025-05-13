df675eb41ceecbe0d40cfbde2407b0d71f4659ba
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-13 21:02:57 +0300

    Test unsupported types

--- tests/UnsupportedTypesTest.php
+++ tests/UnsupportedTypesTest.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Arokettu\Json5\Tests;
 
 use Arokettu\Json5\Json5Encoder;
+use Arokettu\Json5\JsonEncoder;
 use Exception;
 use PHPUnit\Framework\TestCase;
 use TypeError;
 
 class UnsupportedTypesTest extends TestCase
 {
-    public function testResource(): void
+    public function testResourceJson5(): void
     {
         $resource = fopen('php://temp', 'r+');
 
@@ -21,11 +22,29 @@ class UnsupportedTypesTest extends TestCase
         Json5Encoder::encode($resource);
     }
 
-    public function testUnknownObject(): void
+    public function testResourceJson(): void
+    {
+        $resource = fopen('php://temp', 'r+');
+
+        self::expectException(TypeError::class);
+        self::expectExceptionMessage('Unsupported type: resource (stream)');
+
+        JsonEncoder::encode($resource);
+    }
+
+    public function testUnknownObjectJson5(): void
     {
         self::expectException(TypeError::class);
         self::expectExceptionMessage('Unsupported type: Exception');
 
         Json5Encoder::encode(new Exception());
+    }
+
+    public function testUnknownObjectJson(): void
+    {
+        self::expectException(TypeError::class);
+        self::expectExceptionMessage('Unsupported type: Exception');
+
+        JsonEncoder::encode(new Exception());
     }
 }
