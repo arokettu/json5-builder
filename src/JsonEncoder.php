5af940148cb4f22421da5a6b586d7e92cfa30408
Anton Smirnov <sandfox+git@sandfox.me>
2025-05-13 23:00:48 +0300

    Add streams to JSON too

--- src/JsonEncoder.php
+++ src/JsonEncoder.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Arokettu\Json5;
 
+use TypeError;
+
+use function Arokettu\IsResource\try_get_resource_type;
+
 final class JsonEncoder
 {
     public static function encode(mixed $value, Options $options = new Options()): string
@@ -14,5 +18,18 @@ final class JsonEncoder
         $json5 = stream_get_contents($stream);
         fclose($stream);
         return $json5;
+    }
+
+    /**
+     * @param resource $stream
+     * @return resource
+     */
+    public static function encodeToStream($stream, mixed $value, Options $options = new Options())
+    {
+        if (try_get_resource_type($stream) !== 'stream') {
+            throw new TypeError('$stream must be a writable stream');
+        }
+        (new Engine\JsonEngine($value, $options, $stream))->encode();
+        return $stream;
     }
 }
--- tests/EncodingFormatsTest.php
+++ tests/EncodingFormatsTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Arokettu\Json5\Tests;
 
 use Arokettu\Json5\Json5Encoder;
+use Arokettu\Json5\JsonEncoder;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 use TypeError;
@@ -20,12 +21,25 @@ class EncodingFormatsTest extends TestCase
         rewind($resource);
         self::assertEquals("{\n    a: \"b\",\n}\n", stream_get_contents($resource));
         fclose($resource);
+
+        $resource = tmpfile();
+        JsonEncoder::encodeToStream($resource, $data);
+        rewind($resource);
+        self::assertEquals("{\n    \"a\": \"b\"\n}\n", stream_get_contents($resource));
+        fclose($resource);
     }
 
-    public function testNotResource(): void
+    public function testNotResourceJson5(): void
     {
         self::expectException(TypeError::class);
         self::expectExceptionMessage('$stream must be a writable stream');
         Json5Encoder::encodeToStream(new stdClass(), null);
+    }
+
+    public function testNotResourceJson(): void
+    {
+        self::expectException(TypeError::class);
+        self::expectExceptionMessage('$stream must be a writable stream');
+        JsonEncoder::encodeToStream(new stdClass(), null);
     }
 }
